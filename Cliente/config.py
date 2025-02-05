517cbe4a011913aa4abb8200fa3e65f7476200cc
Jarod Lima <143278264+JarodBLima@users.noreply.github.com>
2025-02-05 08:22:09 -0300

    Update config.py

--- Cliente/config.py
+++ Cliente/config.py
@@ -2,7 +2,7 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'chave_secreta'  # Use uma chave secreta forte em produção
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///users.db'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///../instance/users.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     ORDERS_SERVER_HOST = os.environ.get('ORDERS_SERVER_HOST') or 'servidor-pedidos'
     ORDERS_SERVER_PORT = int(os.environ.get('ORDERS_SERVER_PORT') or 50051)
@@ -11,5 +11,5 @@ class Config:
     GRPC_RETRY_MAX_RETRIES = 3
     GRPC_RETRY_DELAY = 2
     CA_CERT_PATH = 'certificados/ca.crt'
-    SSL_CERT_PATH = os.path.join(os.path.dirname(__file__), 'certificados/cliente.crt')
-    SSL_KEY_PATH = os.path.join(os.path.dirname(__file__), 'certificados/cliente.key')
+    SSL_CERT_PATH = 'certificados/cliente.crt'
+    SSL_KEY_PATH = 'certificados/cliente.key'
