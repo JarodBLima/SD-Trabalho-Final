0d12feac2debdad48b583a1877a202f074263e2f
Jarod Lima <143278264+JarodBLima@users.noreply.github.com>
2025-02-07 09:34:06 -0300

    Delete servidor-pagamentos directory

--- servidor-pagamentos/src/java/com/example/pagamento/Pagamento.java
+++
@@ -1,8 +0,0 @@
-package com.example.pagamento;
-
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface Pagamento extends Remote {
-    String processarPagamento(String orderId, double valor) throws RemoteException;
-}
